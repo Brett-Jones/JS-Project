b27a2fba7f264fe0af9808f47864183e72beec6d
Shaharazon Thomas <shaharazonthomas@gmail.com>
2024-06-19 17:53:11 -0400

    updated files

--- index.js
+++ index.js
@@ -20,3 +20,21 @@
       return valString;
     }
   }
+
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+function flipCard() {
+  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+   if (!hasFlippedCard) {
+     hasFlippedCard = true;
+     firstCard = this;
+   }
+}
+  
+  cards.forEach(card => card.addEventListener('click', flipCard));
